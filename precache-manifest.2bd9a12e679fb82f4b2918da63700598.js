d26adf20a5eda64a94e42dae6c25bc793630c095
jsanchez81 <alex_sl7@icloud.com>
2020-04-15 02:14:29 +0200

    Updates

---
+++ precache-manifest.2bd9a12e679fb82f4b2918da63700598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1781d79bdf8f2ba7c0f2f1efb78d002",
+    "url": "/learnReact/index.html"
+  },
+  {
+    "revision": "177220f3597bf3873b8f",
+    "url": "/learnReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f5e427478e820a08149",
+    "url": "/learnReact/static/js/2.0a3367dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learnReact/static/js/2.0a3367dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177220f3597bf3873b8f",
+    "url": "/learnReact/static/js/main.1d980852.chunk.js"
+  },
+  {
+    "revision": "1940a3072c0393a342c9",
+    "url": "/learnReact/static/js/runtime-main.6e932802.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learnReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
